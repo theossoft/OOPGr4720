104d3f5c8f7b4e1096b4593a345af5cad967e196
theos <theossoft@gmail.com>
2023-09-14 22:19:09 +0300

    Домашнее задание 3. Добавлена возможность повтора игры.

--- src/main/java/homeWorks/hw_3/Main.java
+++ src/main/java/homeWorks/hw_3/Main.java
@@ -8,27 +8,40 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        List<Answer> answers = new ArrayList<>();
         Game game = new NumGame();
-        game.start(5,5);
+        game.start(5, 5);
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Введите слово: ");
-        while (!game.getGameStatus().equals(GameStatus.WIN) && !game.getGameStatus().equals(GameStatus.LOSE)){
-            String inputWord = scanner.nextLine();
-            Answer answer = game.inputValue(inputWord);
-            answers.add(answer);
-            System.out.println(answer);
-        }
-        System.out.println(game.getGameStatus());
-        System.out.println("Желаете посмотреть логи игры (Y/N): ");
-        String inputAnswer = scanner.nextLine();
-        if(inputAnswer.equals("Y")) {
-            int index = 1;
-            for (Answer element : answers) {
-                System.out.println(index + " ход. " + element);
-                index++;
+
+        do {
+            System.out.println("Меню игры:\n1. Играть в игру \n2. Выйти из игры");
+            String input = scanner.nextLine();
+            if (input.equals("1")) {
+                List<Answer> answers = new ArrayList<>();
+                System.out.print("Введите слово: ");
+                while (!game.getGameStatus().equals(GameStatus.WIN) && !game.getGameStatus().equals(GameStatus.LOSE)) {
+                    String inputWord = scanner.nextLine();
+                    Answer answer = game.inputValue(inputWord);
+                    answers.add(answer);
+                    System.out.println(answer);
+                }
+                System.out.println(game.getGameStatus());
+                System.out.println("Желаете посмотреть логи игры (Y/N): ");
+                String inputAnswer = scanner.nextLine();
+                if (inputAnswer.equals("Y")) {
+                    int index = 1;
+                    for (Answer element : answers) {
+                        System.out.println(index + " ход. " + element);
+                        index++;
+                    }
+                    System.out.println();
+                }
+            } else if (input.equals("2")) {
+                break;
+            } else {
+                System.out.println("Введите 1 или 2");
             }
 
-        }
+        } while (true);
+
     }
 }
